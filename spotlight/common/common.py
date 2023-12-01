ed5f05020cfc4eb381d1a631af904a0623c98ce7
Park Hyung Won <kinphw@naver.com>
2023-12-01 18:04:16 +0900

    231201_1800

--- spotlight/common/common.py
+++ spotlight/common/common.py
@@ -12,29 +12,3 @@ def obj2str(df:pd.DataFrame) -> None:
         if df[column].dtype == 'object':
             df[column] = df[column].astype(str) # Call by Object Refenece이므로 Return 불필요    
 
-def autoMap(df:pd.DataFrame)->pd.DataFrame :
-
-    print("Auto Mapping. Read ImportMAP.xlsx")
-    #filenameImportMap = "ImportMAP.xlsx"
-    #filenameImportMap = glob.glob(tgtdir+"/"+filenameImportMap)
-    filenameImportMap = myfd.askopenfilename("MAP파일 선택")    
-    dfMap = pd.read_excel(filenameImportMap, sheet_name='MAP_GL')
-
-    ## a. MAP 대상 먼저 전처리
-    dfMapMap = dfMap[dfMap['방법'] == 'MAP']
-    dfTB = pd.DataFrame()
-    for i in range(0, dfMapMap.shape[0]):    
-        try:
-            dfTB[dfMapMap.iloc[i]["tobe"]] = df[dfMapMap.iloc[i]["asis"]]
-        except:
-            dfTB[dfMapMap.iloc[i]["tobe"]] = df[str(dfMapMap.iloc[i]["asis"])]
-
-    ## b. KEYIN 대상 추가 전처리
-    dfMapKeyin = dfMap[dfMap['방법'] == 'KEYIN']
-
-    for i in range(0, dfMapKeyin.shape[0]):        
-        dfTB[dfMapKeyin.iloc[i]["tobe"]] = dfMapKeyin.iloc[i]["asis"]
-
-
-    print("AUTO-MAP Done")
-    return dfTB            
--- spotlight/main.py
+++ spotlight/main.py
@@ -5,6 +5,7 @@ from spotlight.txt2db import runTxt2Db
 from spotlight.concatText import runConcatText
 from spotlight.import2df.import2df import runImport2Df
 from spotlight.save.save import Saver
+from spotlight.automap.automap import AutoMap
 
 class Spotlight:
 
@@ -24,18 +25,22 @@ class Spotlight:
 
         text += "\nMain Run\n"            
         text += "31. Read text to dataframe\n"
-        text += "32. Modify mode\n" #조정자는 별도 클래스로 분리 #아직 미구현
-        text += "33. Auto_MAP\n" # 아직 미연동
+        text += "32. Auto_MAP\n"
+        text += "33. Modify mode(After Auto_MAP)\n" #조정자는 별도 클래스로 분리 #아직 미구현        
 
         text += "\nSave\n"            
         text += "41. Save text(임시파일 Load는 31 활용)\n"
 
+        text += "90. MANUAL HANDLING - DEBUG\n"
         text += "99. df.head(10)"
 
         #####
         textMain = "enter '?' to help / 'q' to exit"
 
         while True:
+
+            print("") #여기다 BREAK를 걸면 디버깅
+
             print(textMain)
             flag = input(">>")
 
@@ -51,15 +56,19 @@ class Spotlight:
                 case '23': runTxt2Db()
 
                 case '31': cls.df = runImport2Df()
+                case '32': cls.df = AutoMap().autoMap(cls.df)
 
                 case '41': Saver().save(cls.df)
 
+                case '90':
+                    print("DEBUG NOW") #여기다 BREAKPOINT를 걸면 수기 디버깅가능
                 case '99': print(cls.df.head(10))
 
                 case _: print("Retry"); continue
 
 def runMain(): 
-    Spotlight().run()
+    spot = Spotlight()
+    spot.run()
 
 if __name__=="__main__":
-    Spotlight().run()
+    runMain()
