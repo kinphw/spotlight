775bec78e7d8df6896616db7b4a9c42643bf61e1
Park <hyungwopark@deloitte.com>
2024-01-28 10:07:53 +0900

    debug

--- spotlight/recon/calSum.py
+++ spotlight/recon/calSum.py
@@ -8,7 +8,7 @@ class CalSum(ReconGL): #기능이 비슷한 recon을 상속  #Inherit Modifier t
     def run(self):
         #cNameAcct = self.selectColumn("Select Account Number (to group by)")
         cNameAmt = self.selectColumn("Select Column to sum")
-        self.export(cNameAmt)
+        self.test(cNameAmt) #240128
 
     def test(self, cNameAmt:str): 
         #먼저 float64로 바꾼다
