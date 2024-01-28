e1c207d5b196542182fea8e3f29cdd18616ebac9
Park Hyung Won <kinphw@naver.com>
2024-01-29 01:12:00 +0900

    merge

--- spotlight/recon/recon.py
+++ spotlight/recon/recon.py
@@ -32,10 +32,10 @@ class ReconGL(Modifier): #Inherit Modifier to use 'selectColumn'
             match(flag):
                 case '1':
                     fileName = input("검증결과 추출할 파일명을 입력하세요(기본값 RECON.xlsx)>>") or 'RECON.xlsx'
-                    df.to_excel(fileName, index=False); break
+                    self.dfResult.to_excel(fileName, index=False); break
                 case '2':
                     fileName = input("검증결과 추출할 파일명을 입력하세요(기본값 RECON.tsv)>>") or 'RECON.tsv'
-                    df.to_csv(fileName, index=False, encoding='utf8', sep='\t'); break
+                    self.dfResult.to_csv(fileName, index=False, encoding='utf8', sep='\t'); break
                 case _:
                     print("잘못 입력하셨습니다.")
 
