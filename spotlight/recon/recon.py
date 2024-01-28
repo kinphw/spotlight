61bd960cb7b3d33f4b393f1077c9984eda3ea35e
Park <hyungwopark@deloitte.com>
2024-01-29 01:05:14 +0900

    0.0.7

--- spotlight/recon/recon.py
+++ spotlight/recon/recon.py
@@ -13,7 +13,7 @@ class ReconGL(Modifier): #Inherit Modifier to use 'selectColumn'
         cNameAcct = self.selectColumn("Select Account Number (to group by)")
         cNameAmt = self.selectColumn("Select Amount LC (to sum)")
         self.test(cNameAcct, cNameAmt)
-        self.export(self)
+        self.export()
         print("DONE")
 
     #상속 후 오버라이드해서 사용할 메서드
@@ -32,10 +32,10 @@ class ReconGL(Modifier): #Inherit Modifier to use 'selectColumn'
             match(flag):
                 case '1':
                     fileName = input("검증결과 추출할 파일명을 입력하세요(기본값 RECON.xlsx)>>") or 'RECON.xlsx'
-                    df.to_excel(fileName, index=False); break
+                    self.dfResult.to_excel(fileName, index=False); break
                 case '2':
                     fileName = input("검증결과 추출할 파일명을 입력하세요(기본값 RECON.tsv)>>") or 'RECON.tsv'
-                    df.to_csv(fileName, index=False, encoding='utf8', sep='\t'); break
+                    self.dfResult.to_csv(fileName, index=False, encoding='utf8', sep='\t'); break
                 case _:
                     print("잘못 입력하셨습니다.")
 
--- spotlight/txt2db/txt2db.py
+++ spotlight/txt2db/txt2db.py
@@ -1,4 +1,5 @@
 import glob
+import time
 
 import pandas as pd
 import tqdm
@@ -9,37 +10,55 @@ from spotlight.txt2db.mod.dbcon import DbCon
 from spotlight.txt2db.mod.txt2df import Txt2Df
 from spotlight.txt2db.mod.df2db import Df2Db
 
-class Txt2Db:
+class Txt2Db():
 
     path : str
     listFiles : list
 
     objEngine : sqlalchemy.Engine
 
-    def run(cls):                
-        cls.path = myfd.askdirectory() #폴더를 input 받는다.        
-        cls.checkFiles() #폴더 내 텍스트 배열을 인식한다.   
-        cls.importAndInsert() #파일을 순환 : import / insert
+    #읽어서 넣기 위한 정보들
+    strEncod:str
+    strSep:str
+    strTableName:str
+    bHeader:bool
+    intChunksize:int
 
-    def checkFiles(cls):
+    def run(self):                
+        self.path = myfd.askdirectory() #폴더를 input 받는다.        
+        self.checkFiles() #폴더 내 텍스트 배열을 인식한다.
+        self.setInfo()
+        
+        timeStart = time.time()
+        self.importAndInsert() #파일을 순환 : import / insert
+        print(time.time() - timeStart, "초 소요")
+
+    def checkFiles(self):
         ext = input("확장자(기본값, tsv)>>") or 'tsv'
-        cls.listFiles = glob.glob(cls.path+'/*.'+ext)
-        print(cls.listFiles)
-    
-    def importAndInsert(cls):
-        cls.objEngine = DbCon().connect() #DB 연결
+        self.listFiles = glob.glob(self.path+'/*.'+ext)
+        print(self.listFiles)
 
-        pbar = tqdm.tqdm(total=len(cls.listFiles))
+    def setInfo(self):
+        self.strEncod = input("ENCODING(기본 UTF8)>>") or 'utf8'
+        self.strSep = input("SEPERATOR(기본 \\t)>>") or '\t'
+        self.strTableName = input("Table Name>>")
+        self.bHeader = (input("text have header? (Y/N)>>") == 'Y')
+        self.intChunksize= int(input("chunksize(기본 500,000)>>") or '500000')
+    
+    def importAndInsert(self):
+        self.objEngine = DbCon().connect() #DB 연결
+
+        pbar = tqdm.tqdm(total=len(self.listFiles))
         objT2D = Txt2Df() #create object to import textfile
-        objD2D = Df2Db(cls.objEngine)
+        objD2D = Df2Db(self.objEngine)
 
         i = 0
-
-        for file in cls.listFiles: #파일을 순환하면서,
+        for file in self.listFiles: #파일을 순환하면서,
             pbar.set_description(file)
 
-            df = objT2D.run(file) #IMPORT
-            objD2D.insert(df, 'lse') #and insert
+            df = objT2D.run(file, self.strSep, self.strEncod, self.bHeader, self.intChunksize) #IMPORT
+            
+            objD2D.insert(df, self.strTableName) #and insert
             
             pbar.update(1)          
             # i += 1
